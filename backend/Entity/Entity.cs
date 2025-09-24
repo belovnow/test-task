8ada899b214d58e4126c1bb424ee18c169254adb
holymosh <@holymosh>
2025-09-24 14:49:48 +0300

    накинул немного для бд

---
+++ backend/Entity/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entity;
+
+public abstract class Entity<TKey>
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public TKey Id { get; set; }
+}
---
+++ backend/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using backend.Database;
+using backend.Entity;
+
+namespace backend.Services;
+
+public class UserService(NspkContext db)
+{
+    public User? GetUser()
+    {
+        return db.Users.FirstOrDefault();
+    }
+}
